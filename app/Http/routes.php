ce535eb9b307e66aa217912ceb662277a70554b8
Marcus David <buthy88@gmail.com>
2015-10-27 21:15:02 -0200

    - Criando controllers para categorias e produtos

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,33 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['prefix' => 'admin'], function() {
+
+    Route::get('categories', ['as'=>'admin/categories', 'uses'=>'AdminCategoriesController@index']);
+    Route::get('products', ['as'=>'admin/products', 'uses'=>'AdminProductsController@index']);
+
+});
+
+/*
+Route::controllers([
+    'auth' => 'Auth\AuthController',
+    'password' => 'Auth\PasswordController'
+]);
+
+Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
+
+    Route::group(['prefix' => 'posts'], function() {
+
+        Route::get('', ['as'=>'admin/posts/index', 'uses'=>'PostsAdminController@index']);
+        Route::get('create', ['as'=>'admin/posts/create', 'uses'=>'PostsAdminController@create']);
+        Route::post('store', ['as'=>'admin/posts/store', 'uses'=>'PostsAdminController@store']);
+        Route::get('edit', ['as'=>'admin/posts/edit', 'uses'=>'PostsAdminController@index']);
+        Route::get('edit/{id}', ['as'=>'admin/posts/edit', 'uses'=>'PostsAdminController@edit']);
+        Route::put('update/{id}', ['as'=>'admin/posts/update', 'uses'=>'PostsAdminController@update']);
+        Route::get('destroy/{id}', ['as'=>'admin/posts/destroy', 'uses'=>'PostsAdminController@destroy']);
+
+    });
+
+});
+*/
